f746a6688773336ce2918d55395d7281e21aa476
dianateper <dianateper333@gmail.com>
2020-11-11 15:23:42 +0200

    Added ploting method

--- app.seed/WebClient/StatisticaDetail.xaml.cs
+++ app.seed/WebClient/StatisticaDetail.xaml.cs
@@ -1,17 +1,11 @@
-﻿using Models.Model;
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using Models.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WebClient
 {
@@ -20,18 +14,89 @@ namespace WebClient
     /// </summary>
     public partial class StatisticaDetail : Window
     {
-        ControllerStatistica statistica; 
+        public Func<double, string> Formatter { get; set; }
+        ControllerStatistica statistica;
+        public SeriesCollection TemperatureCollection { get; set; }
+        public SeriesCollection HumidityCollection { get; set; }
+
 
         public StatisticaDetail(int X, int Y)
         {
             InitializeComponent();
             statistica = MainWindow.channel.GetControllerStatistica(X, Y);
+            ShowDetail(statistica.statisticaTemperature, Temperature);
+            ShowDetail(statistica.statisticaHumidity, Humidity);
+
+            TemperatureCollection = new SeriesCollection
+            {
+                new  LineSeries
+                {
+                    Title = "Temperature",
+                    Values = RawDataSeriesTemperature,
+                    Fill = new SolidColorBrush(Colors.Bisque)
+                }
+            };
+
+            HumidityCollection = new SeriesCollection
+            {
+                new  LineSeries
+                {
+                    Title = "Humidity",
+                    Values = RawDataSeriesHumidity,
+                    Fill = new SolidColorBrush(Colors.Bisque)
+                }
+            };
+            
+            GraphTemperatureAxisX.LabelFormatter = value => new DateTime((long)value).ToString("MM/dd/yy");
+            GraphHumidityAxisX.LabelFormatter = value => new DateTime((long)value).ToString("MM/dd/yy");
+
+            DataContext = this;
+            GraphTemperature.Series = TemperatureCollection;
+            GraphHumidity.Series = HumidityCollection;
         }
 
-        public void ShowDetail()
+        public void ShowDetail(StatisticaView view, System.Windows.Controls.DataGrid grid)
         {
-            
+            grid.Items.Add(new { Property = "Max", Value = view.Max });
+            grid.Items.Add(new { Property = "Min", Value = view.Min });
+            grid.Items.Add(new { Property = "Median", Value = view.Median });
+            grid.Items.Add(new { Property = "Mean", Value = view.Mean });
+            grid.Items.Add(new { Property = "Range", Value = view.Range });
+            grid.Items.Add(new { Property = "Variance", Value = view.Variance });
+            grid.Items.Add(new { Property = "StandartDeriation", Value = view.StandartDeriation });
+            grid.Items.Add(new { Property = "Kurtosis", Value = view.Kurtosis });
+            grid.Items.Add(new { Property = "Skewnes", Value = view.Skewnes });
         }
+
+
+        public ChartValues<DateTimePoint> RawDataSeriesTemperature
+        {
+            get
+            {
+                ChartValues<DateTimePoint> Values = new ChartValues<DateTimePoint>();
+                foreach (ControllerHistory history in statistica.controllerHistories)
+                {
+                    Values.Add(new DateTimePoint(history.datetime, history.temperature));
+                }
+                return Values;
+            }
+        }
+
+        public ChartValues<DateTimePoint> RawDataSeriesHumidity
+        {
+            get
+            {
+                ChartValues<DateTimePoint> Values = new ChartValues<DateTimePoint>();
+                foreach (ControllerHistory history in statistica.controllerHistories)
+                {
+                    Values.Add(new DateTimePoint(history.datetime, history.humidity));
+                }
+                return Values;
+            }
+        }
+
 
     }
+
+
 }
